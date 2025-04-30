2db3bfdc8f2a964bba0e1f9711a60bbdd24ee04e
Artur Zdolinski <artur@zdolinski.com>
2025-04-30 03:00:33 +0200

    init
    	modified:   .github/workflows/docker-image.yml
    	modified:   docker/Dockerfile
    	modified:   docker/src/supervisor/conf.d/simpleserver.conf
    	modified:   src/server/auth/__init__.py
    	modified:   src/server/config.py
    	modified:   src/server/main.py
    	modified:   src/server/routers/__init__.py
    	modified:   src/server/routers/mcp_sse/messages_handlers/initialize_handler.py
    
    	modified:   .github/workflows/docker-image.yml
    	new file:   .github/workflows/test-server.yml
    	modified:   docker/Dockerfile
    	modified:   docker/src/startup/sshd-init.sh
    	renamed:    docker/src/startup/vnc_startup.sh -> docker/src/startup/vnc-init.sh
    	modified:   docker/src/supervisor/conf.d/simpleserver.conf
    	modified:   docker/src/supervisor/conf.d/vnc.conf
    	modified:   src/server/requirements.txt
    	modified:   test/server/conftest.py
    	new file:   test/server/requirements-pytest.txt
    
    	modified:   .github/workflows/docker-image.yml
    	new file:   .github/workflows/test-server.yml
    	modified:   docker/Dockerfile
    	modified:   docker/src/startup/sshd-init.sh
    	renamed:    docker/src/startup/vnc_startup.sh -> docker/src/startup/vnc-init.sh
    	modified:   docker/src/supervisor/conf.d/vnc.conf
    	modified:   src/server/requirements.txt
    	modified:   test/server/conftest.py
    	new file:   test/server/requirements-pytest.txt
    
    	modified:   .github/workflows/docker-image.yml
    	modified:   docker/docker-compose.yaml
    
    	modified:   .github/workflows/docker-image.yml
    	modified:   docker/docker-compose.yaml
    
    	modified:   .github/workflows/docker-image.yml
    	modified:   docker/docker-compose.yaml
    	deleted:    docker/src/startup/chrome-init.sh
    	modified:   docker/src/startup/vnc-init.sh

--- docker/src/startup/vnc-init.sh
+++ docker/src/startup/vnc-init.sh
@@ -64,7 +64,13 @@ cleanup () {
 trap cleanup SIGINT SIGTERM
 
 ## write correct window size to chrome properties
-$STARTUPDIR/chrome-init.sh
+VNC_RES_W=${VNC_RESOLUTION%x*}
+VNC_RES_H=${VNC_RESOLUTION#*x}
+
+echo -e "\n------------------ update chromium-browser.init ------------------"
+echo -e "\n... set window size $VNC_RES_W x $VNC_RES_H as chrome window size!\n"
+
+echo "export CHROMIUM_FLAGS='--no-sandbox --test-type --start-maximized --disable-gpu --disable-dev-shm-usage  --user-data-dir --window-size=$VNC_RES_W,$VNC_RES_H --window-position=0,0'" > $HOME/.chromium-browser.init
 source $HOME/.chromium-browser.init
 
 ## resolve_vnc_connection
